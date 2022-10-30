5d2330652fb2ecf0bba6a0bb7e740c4efd59b670
alimansour0002 <alimansour0002@gmail.com>
2022-10-30 23:26:46 +0300

    bugs fixed

--- src/utils/typeChecker.ts
+++ src/utils/typeChecker.ts
@@ -3,17 +3,17 @@ import { GasTanksType } from '../types';
 import { GasTankProps } from '../types';
 
 export function isGasTankProps(obj: any): obj is GasTankProps {
-    if (typeof obj.name != 'string') return false;
-    if (typeof obj.apiKey != 'string') return false;
-    if (typeof obj.name != 'string') return false;
-    if (obj.chainId == undefined) return false;
-    if (typeof obj.providerURL != 'string') return false;
+    if (typeof obj.name !== 'string') return false;
+    if (typeof obj.apiKey !== 'string') return false;
+    if (typeof obj.name !== 'string') return false;
+    if (obj.chainId === undefined) return false;
+    if (typeof obj.providerURL !== 'string') return false;
     if (!obj?.whiteList?.length) return false;
     return true;
 }
 
 function isGasTanksType(obj: any): obj is GasTanksType {
-    if (obj.length == undefined) return false;
+    if (obj.length === undefined) return false;
     try {
         for (let i = 0; i < obj.length; i++) {
             if (!isGasTankProps(obj[i])) return false;
@@ -24,15 +24,15 @@ function isGasTanksType(obj: any): obj is GasTanksType {
     return true;
 }
 function isDatabaseConfig(obj: any): obj is DatabaseConfig {
-    if (typeof obj.user != 'string') return false;
-    if (typeof obj.host != 'string') return false;
-    if (typeof obj.database != 'string') return false;
-    if (typeof obj.password != 'string') return false;
-    if (typeof obj.port != 'number') return false;
+    if (typeof obj.user !== 'string') return false;
+    if (typeof obj.host !=='string') return false;
+    if (typeof obj.database !== 'string') return false;
+    if (typeof obj.password !== 'string') return false;
+    if (typeof obj.port !== 'number') return false;
     return true;
 }
 
 export function isFileDoc(obj: any): obj is fileDoc {
-    if (obj.whiteList == undefined) return false;
+    if (obj.whiteList === undefined) return false;
     return isDatabaseConfig(obj.databaseConfig) && isGasTanksType(obj.gasTanks);
 }
